d99a308ef160ce087d791b4f666778ba95ebf0e7
Silvia Petruta <silvia_petruta@yahoo.com>
2022-08-30 11:44:42 +0100

    Adding and creating lambda 2nd part.

--- lib/backend-stack.ts
+++ lib/backend-stack.ts
@@ -1,11 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import SubscriptionService2nd from './subscription-service';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    new SubscriptionService2nd(this, 'SubscriptionService2nd');
     // The code that defines your stack goes here
 
     // example resource
---
+++ lib/subscription-service.ts
@@ -0,0 +1,26 @@
+import { Construct } from "constructs";
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as path from 'path';
+
+export default class SubscriptionService2nd extends Construct {
+    constructor(scope: Construct, id: string) {
+        super(scope, id);
+
+        const s3Bucket = new s3.Bucket(this, 'SubscriptionStore2nd');
+
+        const getSubscriptionHandler2nd = new lambda.Function(this, 'GetSubscriptionHandler2nd', {
+            runtime: lambda.Runtime.NODEJS_16_X,
+            handler: 'getSubscription.main',
+            code: lambda.Code.fromAsset(path.join(__dirname, '../resources'))
+        });
+
+        const api = new apigateway.RestApi(this, 'SubscriptionAPI2nd', {
+            restApiName: 'Sunscription-Service-2nd'
+        });
+
+        const getSubcription2nd = new apigateway.LambdaIntegration(getSubscriptionHandler2nd);
+        api.root.addMethod("GET", getSubcription2nd);
+    }
+}
