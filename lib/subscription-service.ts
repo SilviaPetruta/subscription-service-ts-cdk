96a8148d979066dd53fc544447b11a71c68e8f1a
Silvia Petruta <silvia_petruta@yahoo.com>
2022-08-30 15:51:10 +0100

    Drop some of the previous changes

--- lib/subscription-service.ts
+++ lib/subscription-service.ts
@@ -3,23 +3,18 @@ import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as path from 'path';
-
 export default class SubscriptionService2nd extends Construct {
     constructor(scope: Construct, id: string) {
         super(scope, id);
-
         const s3Bucket = new s3.Bucket(this, 'SubscriptionStore2nd');
-
         const getSubscriptionHandler2nd = new lambda.Function(this, 'GetSubscriptionHandler2nd', {
             runtime: lambda.Runtime.NODEJS_16_X,
             handler: 'getSubscription.main',
             code: lambda.Code.fromAsset(path.join(__dirname, '../resources'))
         });
 
-        const api = new apigateway.LambdaRestApi(this, 'SubscriptionAPI2nd', {
-            restApiName: 'Hello World API',
-            handler: getSubscriptionHandler2nd,
-            proxy: false,
+        const api = new apigateway.RestApi(this, 'SubscriptionAPI2nd', {
+            restApiName: 'Sunscription-Service-2nd'
         });
 
         const getSubcription2nd = new apigateway.LambdaIntegration(getSubscriptionHandler2nd);
